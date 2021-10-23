364596d2edc2b4d11838ef58c203f39a365a4edf
Silverlan <silverlann@gmail.com>
2021-10-23 20:16:46 +0200

    Update basic.py

--- tests/basic.py
+++ tests/basic.py
@@ -3,27 +3,31 @@ from pragma_udm_wrapper import UDM, UdmType
 
 if __name__ == '__main__':
     udm = UDM()
-    if udm.load(str(Path.cwd()) +'/tests/test_material_ascii.pmat'):
-        root = udm.root
-        prop = root['pbr']
-        print(prop)
-        prop = root['materialPaths']
-        print(prop)
-        prop = root['mass']
-        print(prop)
-        prop = root['eyeOffset']
-        print(prop)
-        prop = root['materials']
-        print(prop)
-        prop = root['skins[0]/materials']
-        print(prop)
-        prop = root['skeleton/assetData/bones/ValveBiped.Bip01_Pelvis/pose']
-        print(prop)
-        prop = root['collisionMeshes[0]/assetData/vertices']
-        print(prop)
-        prop = root['meshGroups/eli_reference/meshes[0]/subMeshes[0]/assetData/vertices']
-        print(prop)
-        prop = root['meshGroups/eli_reference/meshes[0]/subMeshes[0]/assetType']
-        print(prop)
+    path = str(Path.cwd()) +'/tests/test_material_ascii.pmat'
+    if not udm.load():
+        print 'Failed to load UDM file ' +path +'!'
+        sys.exit(1)
+        
+    root = udm.root
+    prop = root['pbr']
+    print(prop)
+    prop = root['materialPaths']
+    print(prop)
+    prop = root['mass']
+    print(prop)
+    prop = root['eyeOffset']
+    print(prop)
+    prop = root['materials']
+    print(prop)
+    prop = root['skins[0]/materials']
+    print(prop)
+    prop = root['skeleton/assetData/bones/ValveBiped.Bip01_Pelvis/pose']
+    print(prop)
+    prop = root['collisionMeshes[0]/assetData/vertices']
+    print(prop)
+    prop = root['meshGroups/eli_reference/meshes[0]/subMeshes[0]/assetData/vertices']
+    print(prop)
+    prop = root['meshGroups/eli_reference/meshes[0]/subMeshes[0]/assetType']
+    print(prop)
 
-        udm.destroy()
+    udm.destroy()
